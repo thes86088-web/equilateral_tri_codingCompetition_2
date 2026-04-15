cb5f329a38bf2c79409dcaaea3ab8b3444fba5d9
durvesh <thes86088@gmail.com>
2026-04-15 22:55:16 +0530

    divide condition into more modular methods

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
 
-#define left ( col == n-1-row  )
-#define right ( col == n-1+row  )
-#define base ( ( row == n-1 ) && ( col%2 == 1 ) )
-
-def condition( row, col, n ) :
-
-    isLeft = ( col == n-1-row  )
-    isRight = ( col == n-1+row  )
-    isBase = ( ( row == n-1 ) and ( col%2 == 0 ) )
-
-    belongsToPerimeter = isLeft or isRight or isBase
-    
-    isInner = row>0 and row<n-1
-    isInnerLeft = ( col == n-1- (row-2)  ) and isInner
-    isInnerRight = ( col == n-1+(row-2)  ) and isInner
-    isInnerBase = ( ( (row+1) == n-1 ) and ( col%2 == 1 ) and ( col>2 and col<2*n-1 ) )
-
-    belongsToInner = isInnerLeft or isInnerRight or isInnerBase
-    
+def belongsToFinal(row, col, n) : 
     #         row>2*k - 1 and row < n-k
     isFinal = row>3 and row<n-2
     isFinalLeft = ( col == n-1- (row-4)  ) and isFinal
     isFinalRight = ( col == n-1+(row-4)  ) and isFinal
     isFinalBase = ( ( (row+2) == n-1 ) and ( col%2 == 0 ) and (col>4 and col<2*n-2) )
     
-    belongsToFinal = isFinalLeft or isFinalRight or isFinalBase
+    return ( isFinalLeft or isFinalRight or isFinalBase )
+  
+def belongsToInner(row, col, n) :
     
-    if( belongsToFinal ) :
+    isInner = row>0 and row<n-1
+    isInnerLeft = ( col == n-1- (row-2)  ) and isInner
+    isInnerRight = ( col == n-1+(row-2)  ) and isInner
+    isInnerBase = ( ( (row+1) == n-1 ) and ( col%2 == 1 ) and ( col>2 and col<2*n-1 ) )
+
+    return isInnerLeft or isInnerRight or isInnerBase
+    
+def belongsToPerimeter(row, col, n) :
+    
+    isLeft = ( col == n-1-row  )
+    isRight = ( col == n-1+row  )
+    isBase = ( ( row == n-1 ) and ( col%2 == 0 ) )
+
+    return isLeft or isRight or isBase
+   
+
+
+def condition( row, col, n ) :
+    
+    if( belongsToFinal(row, col, n) ) :
         return 3
-    elif( belongsToInner ) :
+    elif( belongsToInner(row, col, n) ) :
         return 2
-    elif (belongsToPerimeter ) :
+    elif (belongsToPerimeter(row, col, n) ) :
         return 1
     else :
         return 0
+
     
 def createCanvas( checkCondition, n ) :
     
